275629c8bff8e9f5a0ca21045b549c114d97b569
Mark Tyers <marktyers@gmail.com>
2016-10-21 20:56:10 +0100

    added stub unit test suite

---
+++ spec/todo-spec.js
@@ -0,0 +1,22 @@
+
+'use strict'
+/*istanbul ignore next*/
+/* global expect */
+
+var todo = require('../todo')
+
+describe('todo', () => {
+
+  beforeEach( () => {
+
+  })
+
+  afterEach( () => {
+
+  })
+
+  it('xxxx', () => {
+    
+  })
+
+})
