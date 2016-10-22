d52c6554ad3a4d95cd87c8d28c490b23f2d5d6c0
Mark Tyers <marktyers@gmail.com>
2016-10-22 19:25:29 +0100

    finished unit tests

--- spec/todo-spec.js
+++ spec/todo-spec.js
@@ -3,20 +3,156 @@
 /*istanbul ignore next*/
 /* global expect */
 
-var todo = require('../todo')
+const lists = require('../modules/lists')
+const globals = require('../modules/globals')
 
-describe('todo', () => {
+const listLen = 3
+const idLen = 31
 
-  beforeEach( () => {
+let auth = {
+	basic: {
+		username: 'testuser',
+		password: 'p455w0rd'
+	}
+}
 
-  })
+const shopping = {
+	name: 'shopping',
+	list: [
+		'Cheese',
+		'Bread',
+		'Butter'
+	]
+}
 
-  afterEach( () => {
+const colours = {
+	name: 'colours',
+	list: [
+		'Red',
+		'Orange',
+		'Yellow',
+		'Green',
+		'Blue'
+	]
+}
 
-  })
+describe('lists', () => {
 
-  it('xxxx', () => {
-    
-  })
+	beforeEach( () => {
+		auth = {
+			basic: {
+				username: 'testuser',
+				password: 'p455w0rd'
+			}
+		}
+	})
+
+	afterEach( () => {
+		lists.clearAll()
+	})
+
+	it('should throw error if missing auth', () => {
+		const res = lists.addNew({}, shopping)
+		expect(res.status).toBe(globals.status.unauthorized)
+		expect(res.format).toBe(globals.format.json)
+		expect(res.message).toBe('missing basic auth')
+	})
+
+	it('should throw error if invalid username', () => {
+		auth.basic.username = 'b4d'
+		const res = lists.addNew(auth, shopping)
+		expect(res.status).toBe(globals.status.unauthorized)
+		expect(res.format).toBe(globals.format.json)
+		expect(res.message).toBe('invalid credentials')
+	})
+
+	it('should throw error if invalid password', () => {
+		auth.basic.password = 'wr0ngp455w0rd'
+		const res = lists.addNew(auth, shopping)
+		expect(res.status).toBe(globals.status.unauthorized)
+		expect(res.format).toBe(globals.format.json)
+		expect(res.message).toBe('invalid credentials')
+	})
+
+	it('should warn if no json body', () => {
+		const res = lists.addNew(auth, '')
+		expect(res.status).toBe(globals.status.badRequest)
+		expect(res.message).toBe('JSON data missing in request body')
+	})
+
+	it('should warn if missing json name key', () => {
+		const res = lists.addNew(auth, {list: ['Matthew', 'Mark', 'Luke', 'John']})
+		expect(res.status).toBe(globals.status.badRequest)
+		expect(res.message).toBe('JSON data missing in request body')
+	})
+
+	it('should warn if missing json list key', () => {
+		const res = lists.addNew(auth, {name: 'Jaberwocky'})
+		expect(res.status).toBe(globals.status.badRequest)
+		expect(res.message).toBe('JSON data missing in request body')
+	})
+
+	it('should warn if list item is not a string', () => {
+		const res = lists.addNew(auth, {name: 'Jaberwocky', list: [42, 3.14]})
+		expect(res.status).toBe(globals.status.badRequest)
+		expect(res.message).toBe('JSON data missing in request body')
+	})
+
+	it('should add a new list', () => {
+		const res = lists.addNew(auth, shopping)
+		expect(res.status).toBe(globals.status.created)
+		expect(res.format).toBe(globals.format.json)
+		expect(res.message).toBe('new list added')
+		expect(res.data.id).toEqual(jasmine.any(String))
+		expect(res.data.id.length).toBe(idLen)
+		expect(res.data.modified).toEqual(jasmine.any(Date))
+		expect(res.data.name).toBe('shopping')
+		expect(res.data.list).toEqual(jasmine.any(Array))
+		expect(res.data.list.length).toBe(listLen)
+	})
+
+	it('should return a list of lists in JSON format', () => {
+		lists.addNew(auth, shopping)
+		lists.addNew(auth, colours)
+		const res = lists.getAll('localhost')
+		//console.log(JSON.stringify(res, null, globals.indent))
+		expect(res.status).toBe(globals.status.ok)
+		expect(res.format).toBe(globals.format.json)
+		expect(res.message).toBe('2 lists found')
+		expect(res.data).toEqual(jasmine.any(Array))
+		expect(res.data[0].name).toBe('shopping')
+		expect(res.data[1].name).toBe('colours')
+	})
+
+	it('should return a list of lists in XML format', () => {
+		lists.addNew(auth, shopping)
+		lists.addNew(auth, colours)
+		const res = lists.getAllXML('localhost')
+		console.log(res)
+	})
+
+	it('should warn if no lists found', () => {
+		const res = lists.getAll('localhost')
+		expect(res.status).toBe(globals.status.notFound)
+		expect(res.message).toBe('no lists found')
+	})
+
+	it('should find a valid list by its id', () => {
+		const added = lists.addNew(auth, shopping)
+		lists.addNew(auth, colours)
+		const res = lists.getByID(added.data.id)
+		expect(res.status).toBe(globals.status.ok)
+		expect(res.data.id).toBe(added.data.id)
+		expect(res.data.name).toBe('shopping')
+		expect(res.data.list.length).toBe(listLen)
+	})
+
+	it('should warn if list by its id not found', () => {
+		lists.addNew(auth, shopping)
+		lists.addNew(auth, colours)
+		const res = lists.getByID('128g4kq9f3qsrwnn4b0q6le')
+		expect(res.status).toBe(globals.status.notFound)
+		expect(res.message).toBe('list not found')
+	})
 
 })
